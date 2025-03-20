c6394aea1549076eed45c01a0bf7c8dd38226811
Arry-Minati <aryakumer.2002.15sep@gmail.com>
2025-03-20 22:15:29 +0530

    added collition scripts and obsticles

--- Assets/Scripts/Mover.cs
+++ Assets/Scripts/Mover.cs
@@ -7,15 +7,26 @@ public class Mover : MonoBehaviour
 
     void Start()
     {
-        
+        PrintIstructions();
     }
 
 
     void Update()
+    {
+        MovePlayer();
+    }
+
+    void PrintIstructions()
+    {
+        Debug.Log("Welcome to the Game!");
+    }
+
+    void MovePlayer()
     {
         float xValue = Input.GetAxis("Horizontal") * Time.deltaTime * moveSpeed;
         float yValue = 0f;
         float zValue = Input.GetAxis("Vertical") * Time.deltaTime * moveSpeed;
         transform.Translate(xValue, yValue, zValue);
     }
+
 }
